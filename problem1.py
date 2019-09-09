21667d27bd951cf78ea9a2920c5c8dd0a198cf8a
Nupur Pandey <mail.nupurpandey@gmail.com>
2019-09-09 16:43:46 +0200

    Solution to first euler problem

---
+++ problem1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9.The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+def getSum():
+	sum = 0
+  	for x in range(1, 1000):
+    	if x % 3 == 0 or x % 5 == 0:
+      	sum +=  x
+  	print "The sum is: ",sum
+
+if __name__ == '__main__':
+  	getSum()
+
